616bae72137a5b4e491dbcf1671cf466261d9e46
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-02 12:26:55 +0100

    search button added

--- src/app/navbars/signin-navbar/signin-navbar.component.ts
+++ src/app/navbars/signin-navbar/signin-navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-signin-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './signin-navbar.component.html',
   styleUrl: './signin-navbar.component.css'
 })
 export class SigninNavbarComponent {
-
+  faSearch = faSearch
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundColor: {
+        cards: '#E9E9E9',
+        search: '#F6F6F6'
+      },
       colors: {
-        headerColor: '3C4242'
+        headerColor: '#3C4242',
+        borderColor: '#8A33FD',
       }
     },
   },
