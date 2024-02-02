c2ecc9c16205759c86e5dc7d27e9ce6218abed20
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-02 12:56:11 +0100

    signin navbar complete

--- src/app/navbars/signin-navbar/signin-navbar.component.ts
+++ src/app/navbars/signin-navbar/signin-navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-signin-navbar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, MatButtonModule],
   templateUrl: './signin-navbar.component.html',
   styleUrl: './signin-navbar.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        cards: '#E9E9E9',
-        search: '#F6F6F6'
+        search: '#F6F6F6',
+        buttonBg: '#8A33FD'
       },
       colors: {
         headerColor: '#3C4242',
         borderColor: '#8A33FD',
+        buttonBorder: '#333333'
       }
     },
   },
