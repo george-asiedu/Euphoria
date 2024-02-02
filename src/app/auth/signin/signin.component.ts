8842797a12f04ee55a788a43518a0d33433ecc0e
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-02 14:43:00 +0100

    signup form updated

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SigninNavbarComponent } from '../../navbars/signin-navbar/signin-navbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [SigninNavbarComponent],
+  imports: [SigninNavbarComponent, RouterLink],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
