5fa8d61763c9aa87151b78b99ca26464380bb3d5
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-05 14:36:27 +0100

    new arrivals card created

--- src/app/home-components/home-page/home-page.component.ts
+++ src/app/home-components/home-page/home-page.component.ts
@@ -3,11 +3,12 @@ import { MainNavbarComponent } from '../../navbars/main-navbar/main-navbar.compo
 import { FooterComponent } from '../../footer/footer.component';
 import { SliderComponent } from '../slider/slider.component';
 import { ExploreComponent } from '../explore/explore.component';
+import { NewArrivalsComponent } from '../new-arrivals/new-arrivals.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [MainNavbarComponent, FooterComponent, SliderComponent, ExploreComponent],
+  imports: [MainNavbarComponent, FooterComponent, SliderComponent, ExploreComponent, NewArrivalsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
