7267774c3930eb064b2cc6f22653563d0f03383a
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-06 16:04:45 +0100

    men products component update

--- src/app/categories/men-products/men-products.component.ts
+++ src/app/categories/men-products/men-products.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenNavbarComponent } from '../../navbars/men-navbar/men-navbar.component';
 import { FooterComponent } from '../../footer/footer.component';
+import { SidenavComponent } from '../../navbars/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-men-products',
   standalone: true,
-  imports: [MenNavbarComponent, FooterComponent],
+  imports: [MenNavbarComponent, FooterComponent, SidenavComponent],
   templateUrl: './men-products.component.html',
   styleUrl: './men-products.component.css'
 })
