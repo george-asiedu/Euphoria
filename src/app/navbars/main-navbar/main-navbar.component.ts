2c83fc7b2de56de11ecba84f8cd49f7777497fb2
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-06 14:19:46 +0100

    Main navbar component updated

--- src/app/navbars/main-navbar/main-navbar.component.ts
+++ src/app/navbars/main-navbar/main-navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-main-navbar',
   standalone: true,
-  imports: [RouterLink, FontAwesomeModule],
+  imports: [RouterLink, FontAwesomeModule, RouterLinkActive],
   templateUrl: './main-navbar.component.html',
   styleUrl: './main-navbar.component.css'
 })
