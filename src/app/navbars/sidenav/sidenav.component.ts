480ab29a2f9da79105c1ec306fb9cc498e1620da
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-06 16:41:00 +0100

    sidenav component updated

--- src/app/navbars/sidenav/sidenav.component.ts
+++ src/app/navbars/sidenav/sidenav.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
 export class SidenavComponent {
-
+  faChevronRight = faChevronRight
+  faChevronUp = faChevronUp
 }
