1a20e3b3fc1aef935e74d83d31470e9967b304df
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-02-07 14:33:06 +0100

    men's product description updated

--- src/app/categories/men/home/home.component.ts
+++ src/app/categories/men/home/home.component.ts
@@ -3,11 +3,12 @@ import { SidenavComponent } from '../../../navbars/sidenav/sidenav.component';
 import { FooterComponent } from '../../../footer/footer.component';
 import { MenNavbarComponent } from '../../../navbars/men-navbar/men-navbar.component';
 import { ProductsComponent } from '../products/products.component';
+import { ProductDescriptionComponent } from '../product-description/product-description.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SidenavComponent, FooterComponent, MenNavbarComponent, ProductsComponent],
+  imports: [SidenavComponent, FooterComponent, MenNavbarComponent, ProductsComponent, ProductDescriptionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
